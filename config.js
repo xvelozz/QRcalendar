6d208df3562d944b7a27f7c7bb8ee6edb7d5d303
Xavier Benigno Veloz Zavala <xvelozz@gmail.com>
2022-09-14 08:26:13 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbwYX2zZxzRJT2tPoafYTL-62RsyzOIMrijIQxVAaAQlxFJ0QyAjUAO5aHbSHJPx_Ngp/exec";
